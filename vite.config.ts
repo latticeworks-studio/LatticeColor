4a6a8d1cdd2fdf476d2464a1106c0522d070a14b
PCPLG\theca <pierre-luc.gagnon@ubisoft.com>
2026-06-04 23:27:22 -0400

    fix: remove overlay.html entry from Vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -14,11 +13,5 @@ export default defineConfig({
     target: ["es2021", "chrome100", "safari13"],
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        overlay: resolve(__dirname, "overlay.html"),
-      },
-    },
   },
 });
